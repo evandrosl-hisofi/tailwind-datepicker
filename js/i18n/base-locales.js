4c7f9c599340a77547f7efae9e0d165013c0d93f
Evandro dos Santos <129754540+evandrosl-hisofi@users.noreply.github.com>
2023-05-26 03:28:04 -0300

    feat: new default languages

--- js/i18n/base-locales.js
+++ js/i18n/base-locales.js
@@ -9,5 +9,28 @@ export const locales = {
     today: "Today",
     clear: "Clear",
     titleFormat: "MM y"
+  },
+  es: {
+    days: ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"],
+    daysShort: ["Dom", "Lun", "Mar", "Mié", "Jue", "Vie", "Sáb"],
+    daysMin: ["Do", "Lu", "Ma", "Mi", "Ju", "Vi", "Sa"],
+    months: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"],
+    monthsShort: ["Ene", "Feb", "Mar", "Abr", "May", "Jun", "Jul", "Ago", "Sep", "Oct", "Nov", "Dic"],
+    today: "Hoy",
+    monthsTitle: "Meses",
+    clear: "Borrar",
+    weekStart: 1,
+    format: "dd/mm/yyyy"
+  },
+  'pt-BR': {
+    days: ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"],
+    daysShort: ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sáb"],
+    daysMin: ["Do", "Se", "Te", "Qu", "Qu", "Se", "Sa"],
+    months: ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"],
+    monthsShort: ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"],
+    today: "Hoje",
+    monthsTitle: "Meses",
+    clear: "Limpar",
+    format: "dd/mm/yyyy"
   }
 };
